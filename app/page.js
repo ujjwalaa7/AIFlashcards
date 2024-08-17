602d0e3972600c31acf11798e11fa416234e432b
Muhammad Amir <umairamir1127@gmail.com>
2024-08-16 23:13:40 -0400

    fixing error for pages

--- app/page.js
+++ app/page.js
@@ -6,9 +6,9 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
 import { auth } from "@/firebase";
 import Link from 'next/link';
-import { Box, Typography, Button, Container } from '@mui/material';
+import { Box, Typography, Button, Container, Divider } from '@mui/material';
 import 'tailwindcss/tailwind.css';
-import Footer from "../app/footer"
+import Footer from "./components/footer";
 
 export default function Home() {
   const [user, loading] = useAuthState(auth);
@@ -29,7 +29,7 @@ if (loading) {
       maxWidth="false"
       className="flex flex-col items-center justify-center min-h-screen"
       style={{ 
-        backgroundImage: 'url(/images/intel-o-flash.png)',
+        backgroundImage: 'url(/images/usethisback.png)',
         backgroundSize: 'cover', 
         backgroundPosition: 'center', 
         backgroundRepeat: 'no-repeat', 
@@ -41,29 +41,56 @@ if (loading) {
         marginLeft: 0,
         padding: 0
       }}
-    >       
-        <Box 
+    >
+    <Box
+      fontFamily={'Comic Sans MS , cursive, sans-serif'}
+      fontWeight= 'bold'
+      fontSize={{ xs: 40, sm: 60, md: 80 }}
+      color={'#1e6c3d'}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ 
+        position: 'relative', 
+        top: { xs: '30%', sm: '25%', md: '25%' },
+        left: { xs: '55%', sm: '55%', md: '50%' },
+        transform: 'translate(-50%, -50%)' 
+      }}
+      >
+       WELCOME TO INTEL-O-FLASH!
+      </Box>   
+    <Box 
     display="flex" 
-    flexDirection="row" 
+    flexDirection={{ xs: 'column', sm: 'row' }}
     gap={3}
     sx={{ 
       position: 'absolute', 
-      top: '40%', 
+      top: { xs: '50%', sm: '40%' },
       left: '50%', 
       transform: 'translate(-50%, -50%)' 
     }}
-  >
+    >
     <Link href='/login' passHref>
-      <Button variant='contained' color='primary' sx={{ minWidth: '120px',backgroundColor: '#004d00', // Dark green
-              '&:hover': {
-                backgroundColor: '#003300' }}}>
+      <Button variant='contained' color='primary' sx={{ 
+        minWidth: '120px',
+        backgroundColor: '#004d00',
+        '&:hover': {
+          backgroundColor: '#003300' 
+        },
+        mb: { xs: 2, sm: 0 }
+      }}>
         Login
       </Button>
     </Link>
     <Link href='/signup' passHref>
-      <Button variant='contained' color='secondary' sx={{ minWidth: '120px', backgroundColor: '#004d00', // Dark green
-              '&:hover': {
-                backgroundColor: '#003300'}}}>
+      <Button variant='contained' color='secondary' sx={{ 
+        minWidth: '120px', 
+        backgroundColor: '#004d00',
+        '&:hover': {
+          backgroundColor: '#003300'
+        }
+      }}>
         Sign Up
       </Button>
     </Link>
